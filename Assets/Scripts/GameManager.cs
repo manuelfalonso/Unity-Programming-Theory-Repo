76d292d0a884ec645d59d1090847ae2c773a0364
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2021-09-14 19:06:05 -0300

    Menu redisigned. GameManager created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private string _playerName;
+    public string PlayerName
+    {
+        get => _playerName;
+        set => _playerName = value;
+    }
+
+    private static GameManager instance;
+    public static GameManager Instance 
+    { 
+        get { return instance; }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+    [SerializeField] private int maxCharNameLength;
+
+    // ENCAPSULATION
+    private string _playerName;
+    public string PlayerName {
+        get { return _playerName; }
+        set
+        {
+            if (value.Length == 0 || value.Length > maxCharNameLength)
+            {
+                throw new ArgumentOutOfRangeException(
+                    $"{nameof(value)} is required and its max length is {maxCharNameLength}");
+            }
+            else
+            {
+                _playerName = value;
+                startButton.interactable = true;
+            }
+        } 
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.interactable = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        GameManager.Instance.PlayerName = PlayerName;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void SetPlayerName(string name)
+    {
+        PlayerName = name;
     }
 }
