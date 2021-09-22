167aa86f6481722c2b2475b736b4b89b22bedab2
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2021-09-22 15:09:32 -0300

    Environment added

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    [SerializeField] private Transform targetPosition;
+    private Animator animator;
+
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float minDistanceToMove = 2f;
+    [SerializeField] private float maxDistnaceToMove = 30f;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Check maximun distance between dog and target
+        if ((targetPosition.position - transform.position).magnitude < maxDistnaceToMove)
+        {
+            // FALSE sit animation
+            animator.SetBool("Sit_b", false);
+
+            // When ready to move
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Locomotion"))
+            {
+                // Linearly increase and decrese between 0 and MaxSpeed
+                float speed = Mathf.PingPong(Time.time, maxSpeed);
+
+                // Check minimun distance with the target
+                if ((targetPosition.position - transform.position).magnitude > minDistanceToMove)
+                {
+                    // FALSE bark animation
+                    animator.SetBool("Bark_b", false);
+
+                    transform.LookAt(targetPosition, Vector3.up);
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+                    // Calculate animation speed
+                    float animationSpeed = Mathf.InverseLerp(0f, maxSpeed, speed);
+                    animator.SetFloat("Speed_f", animationSpeed);
+                }
+                else
+                {
+                    // TRUE bark animation
+                    animator.SetBool("Bark_b", true);
+                }
+            }
+        }
+        else
+        {
+            // TRUE sit animation
+            animator.SetBool("Sit_b", true);
+        }
+    }
+}
